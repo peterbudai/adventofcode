9a2ad0ffbd0fe0873e3d1e54f7ab67099f8cb4ae
Péter Budai <budai@tresorit.com>
2018-12-02 01:49:37 +0100

    Day 1 solutions in Python

---
+++ day1/part1.py
@@ -0,0 +1,2 @@
+with open('day1/input.txt') as f:
+    print(sum((int(line) for line in f), 0))
---
+++ day1/part2.py
@@ -0,0 +1,13 @@
+def find_repeat(input):
+    previous = {0}
+    current = 0
+    while True:
+        for i in input:
+            current += i
+            if current in previous:
+                return current
+            else:
+                previous.add(current)
+
+with open('day1/input.txt') as f:
+    print(find_repeat([int(line) for line in f]))
