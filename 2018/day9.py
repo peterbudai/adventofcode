1b45d1356839434dc7cbbf499510f31c5a371574
Peter Budai <buc@peterbudai.eu>
2018-12-18 19:08:56 +0100

    Faster solution for day 9 part 2

--- 2018/day9.py
+++ 2018/day9.py
@@ -9,24 +9,15 @@ with open('day9.txt') as file:
 
 def high_score(players, marbles):
     circle = deque([0], marbles)
-    length = 1
     players = [0 for _ in range(players)]
-    current = 0
     for marble in range(1, marbles+1):
-        player = marble % len(players)
         if marble % 23 == 0:
-            current = current - 7 if current >= 7 else length - (7 - current)
-            players[player] += marble + circle[current]
-            del circle[current]
-            length -= 1
+            circle.rotate(7)
+            players[marble % len(players)] += marble + circle.popleft()
         else:
-            current = current + 2 if current <= length - 2 else 1
-            circle.insert(current, marble)
-            length += 1
-        # print("[{}] {}".format(player, ''.join("({})".format(c) if i == current else " {} ".format(c) for i,c in enumerate(circle))))
-        if marble % 1000 == 0:
-            print(marble//1000, file=sys.stdout)
+            circle.rotate(-2)
+            circle.appendleft(marble)
     return max(players)
 
 print(high_score(players, marbles))
-#print(high_score(players, marbles * 100))
+print(high_score(players, marbles * 100))
