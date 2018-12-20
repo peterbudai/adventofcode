bb771b9f5c0eb7c4b58afd5a34f90a153a225c08
Péter Budai <buc@peterbudai.eu>
2018-12-20 20:29:41 +0100

    Day 11 faster solution for part2

--- 2018/day11.py
+++ 2018/day11.py
@@ -3,13 +3,16 @@
 with open('day11.txt') as file:
     serial = int(file.read())
 
-grid = [[((x + 10) * y + serial) * (x + 10) % 1000 // 100 - 5 for y in range(1,301)] for x in range(1,301)]
+power = [[[((x + 10) * y + serial) * (x + 10) % 1000 // 100 - 5 for y in range(1,301)] for x in range(1,301)]]
+for s in range(2,301):
+    h = s // 2
+    if s % 2 == 0:
+        power.append([[power[h-1][x][y] + power[h-1][x+h][y] + power[h-1][x][y+h] + power[h-1][x+h][y+h] for y in range(300-s+1)] for x in range(300-s+1)])
+    else:
+        power.append([[power[h-1][x][y] + power[h-1][x+h+1][y] + power[h-1][x][y+h+1] + power[h-1][x+h+1][y+h+1] + sum(power[0][x+h][i] for i in range(y,y+s)) + sum(power[0][i][y+h] for i in range(x,x+s)) - power[0][x+h][y+h] for y in range(300-s+1)] for x in range(300-s+1)])
 
-def region_power(x,y,s):
-    return sum(grid[x+i-1][y+j-1] for j in range(s) for i in range(s))
+def print_coord(c):
+    print(','.join(map(lambda p: str(p+1), c)))
 
-def max_power(s):
-    return max((region_power(x,y,s),x,y) for y in range(1,302-s) for x in range(1,302-s))
-
-print(','.join(str(p) for p in max_power(3)[1:]))
-print(','.join(str(p) for p in max(max_power(s) for s in range(1,301))))
+print_coord(max(((x,y) for x in range(298) for y in range(298)), key=lambda p: power[2][p[0]][p[1]]))
+print_coord(max(((x,y,s) for s in range(301) for x in range(300-s) for y in range(300-s)), key=lambda p: power[p[2]][p[0]][p[1]]))
