9ca946dec14b30f1c12aea4d9e43fc544c18b375
Péter Budai <buc@peterbudai.eu>
2020-12-03 23:32:55 +0100

    Added util tests

--- 2020/src/bin.rs
+++ 2020/src/bin.rs
@@ -1,3 +1,3 @@
 pub fn main() -> anyhow::Result<()> {
-    libaoc::MAIN()
+    aoc::MAIN()
 }
--- 2020/src/util.rs
+++ 2020/src/util.rs
@@ -1,8 +1,7 @@
 //! Utility functions and types for repeating elements of the solutions.
 
 use std::str::FromStr;
-use std::error::Error;
-use std::marker::{Send, Sync};
+use anyhow::Result;
 
 /// Parse line-separated input into a vector. Can convert into anything that
 /// implements the `FromStr` trait.
@@ -18,101 +17,46 @@ use std::marker::{Send, Sync};
 /// # Examples
 /// 
 /// ```
-/// # use libaoc::util::parse_lines;
+/// # use aoc::util::parse_lines;
 /// let input = parse_lines::<u32>("1\n2\n3")?;
 /// assert_eq!(input, [1, 2, 3]);
 /// # anyhow::Result::<()>::Ok(())
 /// ```
-pub fn parse_lines<'a, T>(input: &'a str) -> anyhow::Result<Vec<T>> 
-    where T: FromStr, <T as FromStr>::Err: 'static + Error + Send + Sync
+pub fn parse_lines<'a, T>(input: &'a str) -> Result<Vec<T>, <T as FromStr>::Err> 
+    where T: FromStr
 {
-    Ok(input.lines().map(|line| T::from_str(line)).collect::<Result<Vec<_>, _>>()?)
-}
-
-pub type Coord = (isize, isize);
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Dir {
-    Up = 1,
-    Down = 2,
-    Left = 3,
-    Right = 4,
-}
-
-impl Dir {
-    pub fn delta(&self) -> Coord {
-        match self {
-            Dir::Up => (0, -1),
-            Dir::Down => (0, 1),
-            Dir::Left => (-1, 0),
-            Dir::Right => (1, 0),
-        }
-    }
-
-    pub fn apply(&self, coord: &Coord) -> Coord {
-        let delta = self.delta();
-        (coord.0 + delta.0, coord.1 + delta.1)
-    }
-
-    pub fn apply_mut(&self, coord: &mut Coord) {
-        let delta = self.delta();
-        coord.0 += delta.0;
-        coord.1 += delta.1;
-    }
-
-    pub fn turn(&self, clockwise: bool) -> Self {
-        match self {
-            Dir::Up if clockwise => Dir::Right,
-            Dir::Up => Dir::Left,
-            Dir::Left if clockwise => Dir::Up,
-            Dir::Left => Dir::Down,
-            Dir::Down if clockwise => Dir::Left,
-            Dir::Down => Dir::Right,
-            Dir::Right if clockwise => Dir::Down,
-            Dir::Right => Dir::Up,
-        }
-    }
-
-    pub fn opposite(&self) -> Self {
-        match self {
-            Dir::Up => Dir::Down,
-            Dir::Down => Dir::Up,
-            Dir::Left => Dir::Right,
-            Dir::Right => Dir::Left,
-        }
-    }
-
-    pub fn iter() -> impl Iterator<Item = Dir> {
-        [Dir::Up, Dir::Down, Dir::Left, Dir::Right].iter().copied()
-    }
+    input.lines().map(|line| T::from_str(line)).collect()
 }
 
 #[cfg(test)]
 mod test {
+    use anyhow::Error;
     use super::*;
 
     #[test]
-    fn parse_lines() {
-        //assert_eq!(parse_lines::<chr>("a\nb\nc"), vec!['a','b','c']);
+    fn parse_lines_primitive() -> Result<()> {
+        assert_eq!(parse_lines::<char>("a\nb\nc")?, ['a','b','c']);
+        assert_eq!(parse_lines::<u32>("1\n2\n")?, [1,2]);
+        assert_eq!(parse_lines::<isize>("-5")?, [-5]);
+        assert_eq!(parse_lines::<u8>("")?, []);
+        Ok(())
     }
 
     #[test]
-    fn direction() {
-        let mut c = (-2, 3);
-        let mut d = Dir::Up;
+    fn parse_lines_custom() -> Result<()> {
+        #[derive(Debug, PartialEq)]
+        struct Pair(isize, String);
 
-        for _ in 0..4 {
-            c = d.apply(&c);
-            d = d.turn(true);
-        }
-        assert_eq!(c, (-2, 3));
-        assert_eq!(d, Dir::Up);
+        impl FromStr for Pair {
+            type Err = Error;
 
-        for _ in 0..4 {
-            c = d.apply(&c);
-            d = d.turn(false);
+            fn from_str(s: &str) -> Result<Self> {
+                let parts = s.split_whitespace().collect::<Vec<_>>();
+                Ok(Pair(isize::from_str(parts[0])?, parts[1].to_string()))
+            }
         }
-        assert_eq!(c, (-2, 3));
-        assert_eq!(d, Dir::Up);
+
+        assert_eq!(parse_lines::<Pair>("1 A\n2 B")?, [Pair(1, "A".to_string()), Pair(2, "B".to_string())]);
+        Ok(())
     }
 }
